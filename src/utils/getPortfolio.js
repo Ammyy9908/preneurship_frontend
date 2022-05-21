2c9a9fed8e9949ac7cd994d85b9fdc7c339946a7
Dashank Doshi <72262399+10doshi12@users.noreply.github.com>
2022-05-21 09:37:50 +0530

    up2

--- src/utils/getPortfolio.js
+++ src/utils/getPortfolio.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getPortfolio = async () => {
   try {
-    const r = await axios.get("http://127.0.0.1:8000/api/portfolio/", {
+    const r = await axios.get("https://10doshi12.pythonanywhere.com/api/portfolio/", {
       headers: {
         Authorization: "Token " + localStorage.getItem("access_token"),
       },
